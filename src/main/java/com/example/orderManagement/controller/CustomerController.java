bf4bcdc89623d74035a5a8f71e9df05a3d5327e0
EmanMofeed <khalaf.eman2000@gmail.com>
2023-07-04 21:34:41 +0300

    commit

--- src/main/java/com/example/orderManagement/controller/CustomerController.java
+++ src/main/java/com/example/orderManagement/controller/CustomerController.java
@@ -2,6 +2,8 @@ package com.example.orderManagement.controller;
 
 import com.example.orderManagement.dto.CustomerDto;
 import com.example.orderManagement.service.CustomerService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/customers")
+@Api(tags = "Customers")
 public class CustomerController {
 
     private final CustomerService customerService;
@@ -25,13 +28,13 @@ public class CustomerController {
         CustomerDto createdCustomer = customerService.createCustomer(customerDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(createdCustomer);
     }
-
+    @ApiOperation("Get all customers")
     @GetMapping
     public ResponseEntity<List<CustomerDto>> getAllCustomers() {
         List<CustomerDto> customers = customerService.getAllCustomers();
         return ResponseEntity.ok(customers);
     }
-
+    @ApiOperation("Get a customer by ID")
     @GetMapping("/{id}")
     public ResponseEntity<CustomerDto> getCustomerById(@PathVariable int id) {
         CustomerDto customer = customerService.getCustomerById(id);
